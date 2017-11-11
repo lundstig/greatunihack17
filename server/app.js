044f1d9c0f2b28cfa9eb028eff4d58d5e09afa51
Karl Lundstig <karl.lundstig@gmail.com>
2017-11-11 18:28:33 +0000

    Server now works

--- server/app.js
+++ server/app.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const app = express();
 
+var sips = [];
+var temps = {};
+
 // Parse POSTed data as json
 app.use(express.json());
-
-app.listen(3000, function() {
-  console.log('Supercup server listening on port 3000');
-});
 
 app.get('/', function(req, res) {
   res.send('<html><h1>Super cup</h1></html>');
@@ -18,25 +17,53 @@ app.get('/cup/temp', function(req, res) {
 });
 
 app.post('/cup/temp', function(req, res) {
-  console.log('New temp is ' + req.body.temp);
+  temps[Date.now()] = req.body.temp;
   res.end('ok');
 });
 
+
 app.get('/cup/temp/history', function(req, res) {
+  // Return data from last 10 minutes
+  var now = Date.now();
+  var duration = 10 * 60 * 1000;
+
+  data = Object.keys(temps).filter(time => time > now - duration).
+    reduce((ret, time) => {
+      ret[time] = temps[time]
+      return ret;
+    }, {});
+  
+  res.json({temps: data});
+});
+
+app.get('/cup/temp/historytest', function(req, res) {
   res.json({
     history: {1: 70, 2: 78, 10: 60, 13: 55, 15: 50, 25: 45}
   });
 });
 
-app.get('/cup/sips', function(req, res) {
+app.get('/cup/sips/history', function(req, res) {
+  // Return data from last 10 minutes
+  var now = Date.now();
+  var duration = 10 * 60 * 1000;
+  res.json({
+    sips: sips.filter(time => time > now - duration)
+  });
+});
+
+app.get('/cup/sips/historytest', function(req, res) {
   res.json({timestamps: [1, 2, 3, 4, 5, 7, 9]});
 });
 
 app.post('/cup/sip', function(req, res) {
-  console.log('You sipped!');
+  sips.push(Date.now());
   res.end('ok');
 });
 
 app.get('/cup/commands', function(req, res) {
   res.json({dipping: false});
+});
+
+app.listen(3000, function() {
+  console.log('Supercup server listening on port 3000');
 });
