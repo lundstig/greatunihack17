02c34132964ac1cd30c46a3852255cef1f06969b
Karl Lundstig <karl.lundstig@gmail.com>
2017-11-11 15:42:58 +0000

    Add dummy request handlers

--- server/app.js
+++ server/app.js
@@ -1,10 +1,32 @@
 const express = require('express');
 const app = express();
 
+// Parse POSTed data as json
+app.use(express.json());
+
 app.listen(3000, function() {
   console.log('Supercup server listening on port 3000');
 });
 
 app.get('/', function(req, res) {
-  res.json({temp: 1000, sips: 42});
+  res.send('<html><h1>Super cup</h1></html>');
+});
+
+
+app.get('/cup/temp', function(req, res) {
+  res.json({temp: 1000, ts: Date.now()});
+});
+
+app.post('/cup/temp', function(req, res) {
+  console.log('New temp is ' + req.body.temp);
+  res.end('ok');
+});
+
+app.get('/cup/sip', function(req, res) {
+  res.json({something: yes});
+});
+
+app.post('/cup/sip', function(req, res) {
+  console.log('You sipped!');
+  res.end('ok');
 });
