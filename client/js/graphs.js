f38a04793b2a775f8429f14ca35cd93608ea45c7
Jakub Mandula <jakub@mandula.cz>
2017-11-12 07:42:39 +0000

    add sip graph

--- client/js/graphs.js
+++ client/js/graphs.js
@@ -3,8 +3,11 @@ var baseUrl = "http://" + location.host + "/";
 window.onload = function(){
   console.log("Hello bitchS")
   initTempData();
+  initSipData();
   setInterval(function() {
-    refreshData();
+    refreshTempData();
+    refreshSipData();
+    
   }, 1 * 1000);
 
 }
@@ -97,8 +100,12 @@ function initTempData() {
     var layout = {
       title: 'Temperature',
       xaxis: {
+
         title: 'Time',
-        showgrid: true
+        showgrid: true,
+        //range:[Date.now()-1*60*1000, Date.now()],
+
+        type: 'date'
       },
       yaxis: {
         title: 'Temperature',
@@ -112,7 +119,7 @@ function initTempData() {
 }
 
 
-function refreshData() {
+function refreshTempData() {
   httpGetAsync(baseUrl + "cup/temp/history", function(text) {
     var data = JSON.parse(text);
     var xs = [];
@@ -134,27 +141,93 @@ function refreshData() {
       y: [ys]
     };
     
+
+    if(data.reg != null){
+      var desiredTemp = 68;
+      var timeToEnd = Math.log(desiredTemp/data.reg.coeffs[0])/data.reg.coeffs[1];
+      
+      document.getElementById('teaTime').innerHTML = Math.floor(timeToEnd/60*10)/10;
+      document.getElementById('coeffA').innerHTML = data.reg.coeffs[0]
+      document.getElementById('coeffB').innerHTML = data.reg.coeffs[1];
+    } else {
+      
+    }
+    
+    var update = {
+    
+      'xaxis.range': [Date.now()-10*60*1000, Date.now()],   // updates the xaxis 
+    };
+    Plotly.relayout(tempHistory, update)
+    
+
+    Plotly.extendTraces(tempHistory, data_update, [0]);
+  });
+}
+
+function initSipData(){
+  httpGetAsync(baseUrl + "cup/sips/history", function(text) {
+    var data = JSON.parse(text);
+    var xs = [];
+    var ys = [];
+    
+    var tempHistory = document.getElementById('graph2');
+    console.log(data)
+    data.sips.forEach(function(obj) {
+      var dateD = new Date(parseInt(obj)) ;
+      xs.push(dateD);
+      ys.push(1);
+    });
+
+    var data_update = [{
+      x: xs,
+      y: ys,
+      type:"bar"
+    }];
+    
     var layout = {
-      title: 'Temperature',
+      title: 'Sips',
       xaxis: {
         title: 'Time',
         showgrid: true
       },
       yaxis: {
-        title: 'Temperature',
+        title: 'Sips',
         showgrid: true
       }
       
     }
-    var desiredTemp = 68;
-    var timeToEnd = Math.log(desiredTemp/data.reg.coeffs[0])/data.reg.coeffs[1];
     
-    document.getElementById('teaTime').innerHTML = Math.floor(timeToEnd/60*10)/10;
-    document.getElementById('coeffA').innerHTML = data.reg.coeffs[0]
-    document.getElementById('coeffB').innerHTML = data.reg.coeffs[1];
-    
-    
+    Plotly.newPlot(tempHistory,data_update,layout);
+  });
+}
 
+function refreshSipData(){
+  httpGetAsync(baseUrl + "cup/sips/history", function(text) {
+    var data = JSON.parse(text);
+    var xs = [];
+    var ys = [];
+    
+    var tempHistory = document.getElementById('graph2');
+    console.log(data)
+    data.sips.forEach(function(obj,i) {
+      var dateD = new Date(parseInt(obj)) ;
+      if(tempHistory.data[0].x.map(Number).indexOf(+dateD) != -1){
+        return;
+      }
+      xs.push(dateD);
+      ys.push(1);
+    });
+
+    var data_update = {
+      x: [xs],
+      y: [ys]
+    };
+    
+    var update = {
+    
+      'xaxis.range': [Date.now()-10*60*1000, Date.now()],   // updates the xaxis 
+    };
+    Plotly.relayout(tempHistory, update)
     Plotly.extendTraces(tempHistory, data_update, [0]);
   });
 }
